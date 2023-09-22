d3e8389c0716f778495a1ab82e2021ade0180219
Luis Manuel Hernandez Jimenez <luis.hernandez@grupokonecta.com>
2023-09-21 22:43:23 -0500

    ✨ Se modifica headre

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,16 +4,20 @@ import React, { useEffect } from 'react';
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/navigation'
 
-
 const Header = (name: any) => {
   const cookieName = 'myTokenName';
   const router = useRouter()
-
+  
+  const logout = () => {
+    Cookies.remove(cookieName);
+    router.push('/')
+  }
+  
   useEffect(() => {
     console.log('e')
     // Lógica para eliminar la cookie cuando el componente se monta
     // Cookies.remove(cookieName);
-
+    
     // Opcionalmente, puedes hacer algo después de eliminar la cookie, como redirigir a otra página.
     // Ejemplo de redirección después de eliminar la cookie:
     // window.location.href = '/nueva-pagina';
@@ -21,15 +25,9 @@ const Header = (name: any) => {
 
   return (
     <div className='flex justify-between px-4 pt-4'>
-        <h2>{name}</h2>
+        <h2>name</h2>
         <h2>Welcome Back, Clint</h2>
-        <Button backgroundColor="#1266f1" label="Cerrar sesión" onClick={() => {
-          console.log('Es')
-          console.log(Cookies.get(cookieName));
-          Cookies.remove(cookieName);
-          console.log(Cookies.get(cookieName));
-          router.push('/')
-        }}/>
+        <Button backgroundColor="#1266f1" label="Cerrar sesión" onClick={logout}/>
     </div>
   )
 }
