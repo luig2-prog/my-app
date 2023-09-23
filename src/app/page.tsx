075677d737e77e86d6b007837725f46a145f4f21
Luis Manuel Hernandez Jimenez <luis.hernandez@grupokonecta.com>
2023-09-23 12:25:19 -0500

    ✨ Se modifica imagen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
 import { LoginForm } from '@/components/login-form';
 import Image from 'next/image';
 import React from 'react';
+import img_team from '@/public/team.png';
 
 const login = () => {
   return (
     <section className="h-screen">
       <div className="container h-full px-6 py-24">
         <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
-          <div className="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
+          <div className="flex justify-center md:mb-0 md:w-8/12 lg:w-6/12">
             <Image
-              src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-              className="w-full"
+              src={img_team}
+              className="w-1/2"
               alt="Phone image"
-              width={300}
-              height={300}
             />
           </div>
           <div className="md:w-8/12 lg:ml-6 h-1/2 lg:w-5/12 block rounded-lg bg-white p-6 
