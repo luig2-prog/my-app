7638d024d2f6f5ec339f569c39758bff5bdb47ea
Luis Manuel Hernandez Jimenez <luis.hernandez@grupokonecta.com>
2023-09-27 12:45:13 -0500

    ✨ Se modifica sidebar

--- src/app/dashboard/components/sidebar.tsx
+++ src/app/dashboard/components/sidebar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { RxSketchLogo, RxPerson } from 'react-icons/rx';
 import { HiOutlineShoppingBag } from 'react-icons/hi';
 import { GrConfigure } from 'react-icons/gr';
-import { AiOutlineDropbox, AiOutlineUnorderedList } from 'react-icons/ai';
+import { AiOutlineDropbox, AiOutlineMenu, AiOutlineUnorderedList } from 'react-icons/ai';
 import { FiSettings } from 'react-icons/fi';
 import { FaUsers } from 'react-icons/fa';
 import { FcDataConfiguration } from 'react-icons/fc';
@@ -26,161 +26,173 @@ const SidebarApp: React.FC<LayoutProps> = ({ children }) => {
     setIsSidebarOpen(!isSidebarOpen);
   };
   return (
-    <div className='grid grid-cols-1'>
-      <button
-        onClick={toggleSidebar}
-        className="msd:hidden lsg:hidden fixed top-4 left-4 bg-red-500 text-white p-2 rounded-full"
-      >Donde estoy</button>
-      <Link href='/dashboard'className='w-16'>
-        <div className='bg-[#1266f1] text-white p-3 rounded-lg inline-block'>
-          <RxSketchLogo size={20} />
+    <div className='flex'>
+      <div className={`grid ${isSidebarOpen ? '' : 'w-10'}`}>
+        <div className={`grid ${isSidebarOpen ? 'grid-cols-2' : ' w-10'}`}>
+          <Link href='/dashboard' className='w-11 h-11'>
+            <div className='bg-[#1266f1] text-white p-3 rounded-lg inline-block'>
+              <RxSketchLogo size={20} />
+            </div>
+          </Link>
+          <button
+            onClick={toggleSidebar}
+            className="msd:hidden lsg:hidden fidxed bg-resd-500 text-wshite p-2 rounded-full"
+          >
+           <AiOutlineMenu size={20} />
+          </button>
         </div>
-      </Link>
-      <div className={`w-16 md:w-48 lg:w-48 h-full pt-4 p-2 bg-white border-r-[1px] flex flex-col justify-between transform transition-transform duration-300 ease-in-out ${isSidebarOpen ? '' : '-translate-x-full'}`}>
-        <div className='flex flex-col items-center'>
-          <Link href='/dashboard/config-app' className='w-full flex justify-between 
+        <div className={`w-16 md:w-48 lg:w-48 h-full pt-4 p-2 bg-white border-r-[1px] flex \
+          flex-col justify-between transform transition-transform duration-300 ease-in-out 
+          ${isSidebarOpen ? '' : '-translate-x-full'}`}>
+          <div className='flex flex-col items-center'>
+            <Link href='/dashboard/config-app' className='w-full flex justify-between 
             hover:bg-gray-200 cursor-pointer p-1 rounded-lg text-center'>
-            <div className='inline-block'>
-              <FcDataConfiguration size={20} />
-            </div>
-            <span className='pl-1 pr-1 pb-1'>Configuración del sistema</span>
-            <div className='inline-block mr-2'>
-              <IoChevronDownOutline size={20} />
-            </div>
-          </Link>
-          <Link href='/dashboard/config' className='w-full flex justify-between 
+              <div className='inline-block'>
+                <FcDataConfiguration size={20} />
+              </div>
+              <span className='pl-1 pr-1 pb-1'>Configuración del sistema</span>
+              <div className='inline-block mr-2'>
+                <IoChevronDownOutline size={20} />
+              </div>
+            </Link>
+            <Link href='/dashboard/config' className='w-full flex justify-between 
             hover:bg-gray-200 cursor-pointer p-1 rounded-lg text-center'>
-            <div className='inline-block'>
-              <AiOutlineDropbox size={20} />
-            </div>
-            <span className='pl-1 pr-1 pb-1'>My workbech</span>
-            <div className='inline-block mr-2'>
-              <IoChevronDownOutline size={20} />
-            </div>
-          </Link>
-          <Link href='/dashboard/users' className='w-full flex justify-between 
+              <div className='inline-block'>
+                <AiOutlineDropbox size={20} />
+              </div>
+              <span className='pl-1 pr-1 pb-1'>My workbech</span>
+              <div className='inline-block mr-2'>
+                <IoChevronDownOutline size={20} />
+              </div>
+            </Link>
+            <Link href='/dashboard/users' className='w-full flex justify-between 
             hover:bg-gray-200 cursor-pointer p-1 rounded-lg text-center'>
-            <div className='inline-block'>
-              <RxPerson size={20} />
-            </div>
-            <span className='pl-1 pr-1 pb-1'>Usuarios</span>
-            <div className='inline-block mr-2'>
-              <IoChevronDownOutline size={20} />
-            </div>
-          </Link>
-          <Link href='/dashboard/customers' className='w-full flex justify-between 
+              <div className='inline-block'>
+                <RxPerson size={20} />
+              </div>
+              <span className='pl-1 pr-1 pb-1'>Usuarios</span>
+              <div className='inline-block mr-2'>
+                <IoChevronDownOutline size={20} />
+              </div>
+            </Link>
+            <Link href='/dashboard/customers' className='w-full flex justify-between 
             hover:bg-gray-200 cursor-pointer p-1 rounded-lg text-center'>
-            <div className='inline-block'>
-              <FaUsers size={20} />
-            </div>
-            <span className='pl-1 pr-1 pb-1'>Roles</span>
-            <div className='inline-block mr-2'>
-              <IoChevronDownOutline size={20} />
-            </div>
-          </Link>
-          <Link href='/dashboard/customers' className='w-full flex justify-between 
+              <div className='inline-block'>
+                <FaUsers size={20} />
+              </div>
+              <span className='pl-1 pr-1 pb-1'>Roles</span>
+              <div className='inline-block mr-2'>
+                <IoChevronDownOutline size={20} />
+              </div>
+            </Link>
+            <Link href='/dashboard/customers' className='w-full flex justify-between 
             hover:bg-gray-200 cursor-pointer p-1 rounded-lg text-center'>
-            <div className='inline-block'>
-              <SiGoogletagmanager size={20} />
-            </div>
-            <span className='pl-1 pr-1 pb-1'>Gestión</span>
-            <div className='inline-block mr-2'>
-              <IoChevronDownOutline size={20} />
-            </div>
-          </Link>
-          <Link href='/dashboard/states' className='w-full flex justify-between 
+              <div className='inline-block'>
+                <SiGoogletagmanager size={20} />
+              </div>
+              <span className='pl-1 pr-1 pb-1'>Gestión</span>
+              <div className='inline-block mr-2'>
+                <IoChevronDownOutline size={20} />
+              </div>
+            </Link>
+            <Link href='/dashboard/states' className='w-full flex justify-between 
             hover:bg-gray-200 cursor-pointer p-1 rounded-lg text-center'>
-            <div className='inline-block'>
-              <TiFlowMerge size={20} />
-            </div>
-            <span className='pl-1 pr-1 pb-1'>Estados</span>
-            <div className='inline-block mr-2'>
-              <IoChevronDownOutline size={20} />
-            </div>
-          </Link>
-          <Link href='/dashboard/customers' className='w-full flex justify-between 
+              <div className='inline-block'>
+                <TiFlowMerge size={20} />
+              </div>
+              <span className='pl-1 pr-1 pb-1'>Estados</span>
+              <div className='inline-block mr-2'>
+                <IoChevronDownOutline size={20} />
+              </div>
+            </Link>
+            <Link href='/dashboard/customers' className='w-full flex justify-between 
             hover:bg-gray-200 cursor-pointer p-1 rounded-lg text-center'>
-            <div className='inline-block'>
-              <TbTemplate size={20} />
-            </div>
-            <span className='pl-1 pr-1 pb-1'>Plantilla de trabajo</span>
-            <div className='inline-block mr-2'>
-              <IoChevronDownOutline size={20} />
-            </div>
-          </Link>
-          <Link href='/dashboard/config' className='w-full flex justify-between 
+              <div className='inline-block'>
+                <TbTemplate size={20} />
+              </div>
+              <span className='pl-1 pr-1 pb-1'>Plantilla de trabajo</span>
+              <div className='inline-block mr-2'>
+                <IoChevronDownOutline size={20} />
+              </div>
+            </Link>
+            <Link href='/dashboard/config' className='w-full flex justify-between 
             hover:bg-gray-200 cursor-pointer p-1 rounded-lg text-center'>
-            <div className='inline-block'>
-              <GrConfigure size={20} />
-            </div>
-            <span className='pl-1 pr-1 pb-1'>Párametros</span>
-            <div className='inline-block mr-2'>
-              <IoChevronDownOutline size={20} />
-            </div>
-          </Link>
-          <Link href='/dashboard/customers' className='w-full flex justify-between 
+              <div className='inline-block'>
+                <GrConfigure size={20} />
+              </div>
+              <span className='pl-1 pr-1 pb-1'>Párametros</span>
+              <div className='inline-block mr-2'>
+                <IoChevronDownOutline size={20} />
+              </div>
+            </Link>
+            <Link href='/dashboard/customers' className='w-full flex justify-between 
             hover:bg-gray-200 cursor-pointer p-1 rounded-lg text-center'>
-            <div className='inline-block'>
-              <IoBusinessOutline size={20} />
-            </div>
-            <span className='pl-1 pr-1 pb-1'>Regla de negocio</span>
-            <div className='inline-block mr-2'>
-              <IoChevronDownOutline size={20} />
-            </div>
-          </Link>
-          <Link href='/dashboard/customers' className='w-full flex justify-between 
+              <div className='inline-block'>
+                <IoBusinessOutline size={20} />
+              </div>
+              <span className='pl-1 pr-1 pb-1'>Regla de negocio</span>
+              <div className='inline-block mr-2'>
+                <IoChevronDownOutline size={20} />
+              </div>
+            </Link>
+            <Link href='/dashboard/customers' className='w-full flex justify-between 
             hover:bg-gray-200 cursor-pointer p-1 rounded-lg text-center'>
-            <div className='inline-block'>
-              <AiOutlineUnorderedList size={20} />
-            </div>
-            <span className='pl-1 pr-1 pb-1'>Listas</span>
-            <div className='inline-block mr-2'>
-              <IoChevronDownOutline size={20} />
-            </div>
-          </Link>
-          <Link href='/dashboard/customers' className='w-full flex justify-between 
+              <div className='inline-block'>
+                <AiOutlineUnorderedList size={20} />
+              </div>
+              <span className='pl-1 pr-1 pb-1'>Listas</span>
+              <div className='inline-block mr-2'>
+                <IoChevronDownOutline size={20} />
+              </div>
+            </Link>
+            <Link href='/dashboard/customers' className='w-full flex justify-between 
             hover:bg-gray-200 cursor-pointer p-1 rounded-lg text-center'>
-            <div className='inline-block'>
-              <TbReportSearch size={20} />
-            </div>
-            <span className='pl-1 pr-1 pb-1'>Reportes</span>
-            <div className='inline-block mr-2'>
-              <IoChevronDownOutline size={20} />
-            </div>
-          </Link>
-          <Link href='/dashboard/customers' className='w-full flex justify-between 
+              <div className='inline-block'>
+                <TbReportSearch size={20} />
+              </div>
+              <span className='pl-1 pr-1 pb-1'>Reportes</span>
+              <div className='inline-block mr-2'>
+                <IoChevronDownOutline size={20} />
+              </div>
+            </Link>
+            <Link href='/dashboard/customers' className='w-full flex justify-between 
             hover:bg-gray-200 cursor-pointer p-1 rounded-lg text-center'>
-            <div className='inline-block'>
-              <BsFillCloudUploadFill size={20} />
-            </div>
-            <span className='pl-1 pr-1 pb-1'>Carga de archivos</span>
-            <div className='inline-block mr-2'>
-              <IoChevronDownOutline size={20} />
-            </div>
-          </Link>
-          <Link href='/dashboard/customers' className='w-full flex justify-between 
+              <div className='inline-block'>
+                <BsFillCloudUploadFill size={20} />
+              </div>
+              <span className='pl-1 pr-1 pb-1'>Carga de archivos</span>
+              <div className='inline-block mr-2'>
+                <IoChevronDownOutline size={20} />
+              </div>
+            </Link>
+            <Link href='/dashboard/customers' className='w-full flex justify-between 
             hover:bg-gray-200 cursor-pointer p-1 rounded-lg text-center'>
-            <div className='inline-block'>
-              <HiOutlineShoppingBag size={20} />
-            </div>
-            <span className='pl-1 pr-1 pb-1'>Customers</span>
-            <div className='inline-block mr-2'>
-              <IoChevronDownOutline size={20} />
-            </div>
-          </Link>
-          <Link href='/dashboard/flows' className='w-full flex justify-between 
+              <div className='inline-block'>
+                <HiOutlineShoppingBag size={20} />
+              </div>
+              <span className='pl-1 pr-1 pb-1'>Customers</span>
+              <div className='inline-block mr-2'>
+                <IoChevronDownOutline size={20} />
+              </div>
+            </Link>
+            <Link href='/dashboard/flows' className='w-full flex justify-between 
             hover:bg-gray-200 cursor-pointer p-1 rounded-lg text-center'>
-            <div className='inline-block'>
-              <FiSettings size={20} />
-            </div>
-            <span className='pl-1 pr-1 pb-1'>Flows</span>
-            <div className='inline-block mr-2'>
-              <IoChevronDownOutline size={20} />
-            </div>
-          </Link>
+              <div className='inline-block'>
+                <FiSettings size={20} />
+              </div>
+              <span className='pl-1 pr-1 pb-1'>Flows</span>
+              <div className='inline-block mr-2'>
+                <IoChevronDownOutline size={20} />
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
-      <main className={`transform transition-transform duration-400 ease-in-out ${isSidebarOpen ? 'ml-48 translate-x-4' : ''}`}>{children}</main>
+      <main className={`transform transition-transform duration-400 ease-in-out 
+        ${isSidebarOpen ? 'translate-x-1' : ''}`}
+      >
+        {children}
+      </main>
     </div>
   );
 };
