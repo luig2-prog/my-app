7538b06ff761f530912443af018cec14aebf7a9d
Luis Manuel Hernandez Jimenez <luis.hernandez@grupokonecta.com>
2023-09-26 19:27:52 -0500

    💄  Se modifica página 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,26 +1,28 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import img_avatar_not_found from '@/public/avatar-not-found.svg'
 
 export default function NotFound() {
   return (
-    <div className="bg-white flex-row justify-center w-full">
-      <div className="bg-white h-[372px] relative flex items-center justify-center">
-        <div className="absolute w-full h-[38px] top-0 left-0 bg-[#708fff]">
-          <div className="absolute top-[9px] left-[239px] [font-family:'Inter-Regular',_Helvetica] font-normal text-white text-[16px] tracking-[0] leading-[normal] whitespace-nowrap">
-            
-          </div>
-        </div>
-        <img className="absolute h-[121px] top-[93px]" alt="Fatal error" src="fatal-error.png" />
-        <div className="absolute top-[216px] [font-family:'Inter-SemiBold',_Helvetica] font-semibold text-black text-[24px] tracking-[0] leading-[normal]">
-          OOPS!
-        </div>
-        <p className="absolute top-[251px] [font-family:'Inter-Medium',_Helvetica] font-medium text-[#504f4f] text-[13px] tracking-[0] leading-[normal]">
-          There is no account found. Please click below to add your account
-        </p>
-        <div className="absolute w-[161px] h-[22px] top-[278px] bg-[#5f82ff] rounded-[4px]">
-          <Link href="/dashboard" className="flex justify-center align-middle [font-family:'Inter-SemiBold',_Helvetica] font-semibold    text-white text-[13px] tracking-[0] leading-[normal] text-center">
-            Return Home
+    <div className="h-screen w-screen bg-gray-50 flex items-center">
+      <div className="container flex flex-col md:flex-row items-center justify-between px-5 text-gray-700">
+        <div className="w-full lg:w-1/2 mx-8">
+          <div className="text-7xl text-purple-700 font-dark font-extrabold mb-8"> 404</div>
+          <p className="text-2xl md:text-3xl font-light leading-normal mb-8">
+            Lo sentimos, no pudimos encontrar la página que estás buscando.
+          </p>
+
+          <Link href="/dashboard" className="px-5 inline py-3 text-sm font-medium leading-5 
+            shadow-2xl text-white transition-all duration-400 border border-transparent 
+            rounded-lg focus:outline-none bg-purple-700 active:bg-red-600 hover:bg-purple-950">
+            back to homepage
           </Link>
         </div>
+        <div className="w-full lg:flex lg:justify-end lg:w-1/2 mx-5 my-12">
+          <Image src={img_avatar_not_found} className="" alt="Page not found"
+          />
+        </div>
+
       </div>
     </div>
   )
